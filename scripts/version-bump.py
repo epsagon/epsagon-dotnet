4724af9286af2f2ab35899ea0e31a55a2a285a7c
Tal Vintrob <tal@epsagon.com>
2019-08-20 10:00:38 +0300

    feat(packaging): added option to set a version instead of bumping

--- scripts/version-bump.py
+++ scripts/version-bump.py
@@ -6,6 +6,7 @@ import xml.etree.ElementTree as ET
 parser = ArgumentParser()
 parser.add_argument('--type', help="type of bump (major, minor, patch)")
 parser.add_argument('--proj', help="path to the .csproj file to update")
+parser.add_argument('--set-version', help="explicitly set a version number")
 args = parser.parse_args()
 
 tree = ET.parse(args.proj)
@@ -13,11 +14,16 @@ root = tree.getroot()
 version = root.findall('./PropertyGroup/Version')[0]
 major, minor, patch = [int(x) for x in version.text.split('.')]
 
-if args.type == 'major': major += 1
-elif args.type == 'minor': minor += 1
-elif args.type == 'patch': patch += 1
+if args.type == 'major':
+    major += 1
+elif args.type == 'minor':
+    minor += 1
+elif args.type == 'patch':
+    patch += 1
 
-version.text = '.'.join(str(x) for x in (major, minor, patch))
+version.text = ('{}.{}.{}'.format(major, minor, patch)
+                if not args.set_version
+                else args.set_version)
 tree.write(args.proj)
 
 print(f"(version-bump): {args.proj} -> {version.text}")
