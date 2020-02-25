351d662b690d8714efc30825dd1a41863533eb07
Tal Vintrob <tal@epsagon.com>
2020-02-25 18:52:34 +0200

    fix(empty-handler-event): added resource name and type to empty handler

--- src/Epsagon.Dotnet.Instrumentation/Handlers/Empty/EmptyHandler.cs
+++ src/Epsagon.Dotnet.Instrumentation/Handlers/Empty/EmptyHandler.cs
@@ -7,6 +7,8 @@ namespace Epsagon.Dotnet.Instrumentation.Handlers.Empty
     {
         public override void HandleAfter(IExecutionContext executionContext, IScope scope)
         {
+            scope.Span.SetTag("resource.name", executionContext.RequestContext.ServiceMetaData.ServiceId);
+            scope.Span.SetTag("resource.type", executionContext.RequestContext.ServiceMetaData.ServiceId);
         }
 
         public override void HandleBefore(IExecutionContext executionContext, IScope scope)
