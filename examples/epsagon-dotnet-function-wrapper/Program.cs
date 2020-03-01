35b69b3fc2224edcd2ee75431f0b2ed274fda21b
Tal Vintrob <tal@epsagon.com>
2020-03-01 18:11:01 +0200

    feat(examples): added general wrapper usage example

---
+++ examples/epsagon-dotnet-function-wrapper/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Epsagon.Dotnet.Lambda;
+
+namespace EpsagonDotnetFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EpsagonBootstrap.Bootstrap();
+
+            var invoker = new LambdaInvoker();
+            var responseTask = invoker.InvokeLambda(args[0]);
+            Console.WriteLine($"Lambda Result: {responseTask}");
+        }
+    }
+}
