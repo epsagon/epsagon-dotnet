a654a932ae0f2475dc470ed6b637f633e57ce71b
Tal Vintrob <tal@epsagon.com>
2020-03-08 11:23:05 +0200

    fix: fixed SQS no resource.name

--- src/Epsagon.Dotnet.Instrumentation/Handlers/SQS/Operations/ReceiveMessageRequestHandler.cs
+++ src/Epsagon.Dotnet.Instrumentation/Handlers/SQS/Operations/ReceiveMessageRequestHandler.cs
@@ -7,19 +7,29 @@ namespace Epsagon.Dotnet.Instrumentation.Handlers.SQS.Operations
 {
     public class ReceiveMessageRequestHandler : IOperationHandler
     {
-        public string QueueName { get; set; }
-
         public void HandleOperationAfter(IExecutionContext context, IScope scope)
         {
+            var request = context.RequestContext.OriginalRequest as ReceiveMessageRequest;
             var response = context.ResponseContext.Response as ReceiveMessageResponse;
             var messages = response.Messages;
 
+            scope.Span.SetTag("aws.sqs.messages_count", messages.Count);
+
             foreach (var message in messages)
             {
-                using (var messageScope = GlobalTracer.Instance.BuildSpan("ReceiveMessage").StartActive(finishSpanOnDispose: true))
+                using (var messageScope = GlobalTracer.Instance
+                    .BuildSpan("ReceiveMessage")
+                    .AsChildOf(scope.Span.Context)
+                    .StartActive(finishSpanOnDispose: true))
                 {
-                    messageScope.Span.SetTag("resource.name", QueueName);
+                    string queueName;
+                    try { queueName = request.QueueUrl.Split('/')[4]; }
+                    catch { queueName = "Invalid Queue Name"; }
+
+                    messageScope.Span.SetTag("resource.name", queueName);
                     messageScope.Span.SetTag("resource.type", "sqs");
+                    messageScope.Span.SetTag("event.origin", "aws-sdk");
+                    messageScope.Span.SetTag("aws.sqs.queue.url", request.QueueUrl);
                     messageScope.Span.SetTag("aws.sqs.message_body", message.Body);
                     messageScope.Span.SetTag("aws.sqs.message_id", message.MessageId);
                     messageScope.Span.SetTag("aws.sqs.message_body_md5", message.MD5OfBody);
@@ -31,9 +41,12 @@ namespace Epsagon.Dotnet.Instrumentation.Handlers.SQS.Operations
         {
             var request = context.RequestContext.OriginalRequest as ReceiveMessageRequest;
 
-            QueueName = "Invalid Queue Name";
-            try { QueueName = request.QueueUrl.Split('/')[4]; }
-            catch { scope.Span.SetTag("aws.sqs.queue.url", request.QueueUrl); }
+            string queueName;
+            try { queueName = request.QueueUrl.Split('/')[4]; }
+            catch { queueName = "Invalid Queue Name"; }
+
+            scope.Span.SetTag("resource.name", queueName);
+            scope.Span.SetTag("resource.type", "sqs");
         }
     }
 }
