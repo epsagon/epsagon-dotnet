7915b8cdc4a5dbf603c19eebb324afa60d074273
Tal Vintrob <tal@epsagon.com>
2020-03-09 14:18:08 +0200

    examples: added elastic example

---
+++ examples/epsagon-dotnet-elasticsearch-app/EpsagonDotnetElasticSearch/Person.cs
@@ -0,0 +1,9 @@
+namespace EpsagonDotnetElasticSearch
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ examples/epsagon-dotnet-elasticsearch-app/EpsagonDotnetElasticSearch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Nest;
+using Epsagon.Dotnet.Instrumentation.ElasticSearch;
+
+namespace EpsagonDotnetElasticSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var settings = new ConnectionSettings()
+                .DefaultIndex("people")
+                .UseEpasgon();
+
+            var client = new ElasticClient(settings);
+            var results = client.Search<Person>(s => s
+                .Query(q => q
+                    .Match(m => m
+                        .Field(f => f.Age)
+                        .Query("25")
+                    )
+                )
+            );
+
+            System.Console.WriteLine(
+                string.Join(
+                    Environment.NewLine,
+                    results.Documents.Select(p => $"[{p.Id}] {p.Name} ({p.Age})")
+                )
+            );
+        }
+    }
+}
