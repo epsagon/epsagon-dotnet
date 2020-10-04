ee01806f50d061f239fc2331f1d9bb9f32d7cec9
Ido Nava <idonava@epsagon.com>
2020-10-04 12:07:15 +0300

    feat(legacy-traces): add getTraceUrl to handlers

--- src/Epsagon.Dotnet.Mvc/EpsagonMvcHandler.cs
+++ src/Epsagon.Dotnet.Mvc/EpsagonMvcHandler.cs
@@ -3,6 +3,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Epsagon.Dotnet.Core;
+using Epsagon.Dotnet.Instrumentation;
 using Epsagon.Dotnet.Tracing.Legacy;
 using Epsagon.Dotnet.Tracing.OpenTracingJaeger;
 using Microsoft.AspNetCore.Http;
@@ -26,6 +27,7 @@ namespace Epsagon.Dotnet.Mvc
             var trace = EpsagonConverter.CreateTrace(JaegerTracer.GetSpans());
             EpsagonTrace.SendTrace(trace);
             JaegerTracer.Clear();
+            EpsagonUtils.ClearTraceUrl();
             return result;
         }
 
@@ -53,11 +55,15 @@ namespace Epsagon.Dotnet.Mvc
         private static IScope CreateRunner(HttpContext context)
         {
             var scope = GlobalTracer.Instance.BuildSpan("invoke").StartActive(finishSpanOnDispose: true);
+            string traceId = Guid.NewGuid().ToString();
+            string startTime = ((int) DateTime.UtcNow.ToUnixTime()).ToString();
             scope.Span.SetTag("event.id", Guid.NewGuid().ToString());
             scope.Span.SetTag("event.origin", "runner");
             scope.Span.SetTag("resource.name", context.Request.Host.Value);
             scope.Span.SetTag("resource.type", "dotnet_function");
             scope.Span.SetTag("resource.operation", context.Request.Method.ToString());
+            scope.Span.SetTag("trace_id", traceId);
+            EpsagonUtils.SetTraceUrl(traceId, startTime);
             return scope;
         }
     }
