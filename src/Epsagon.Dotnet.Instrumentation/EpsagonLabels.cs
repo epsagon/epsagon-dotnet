e62bf530a8e6cc0cfbc22b1140b96b0482e28008
James Eastham <j.eastham1993@gmail.com>
2021-02-23 15:04:01 +0000

    chore(formatting): fix method call formatting (#36)

--- src/Epsagon.Dotnet.Instrumentation/EpsagonLabels.cs
+++ src/Epsagon.Dotnet.Instrumentation/EpsagonLabels.cs
@@ -9,30 +9,22 @@ namespace Epsagon.Dotnet.Instrumentation
 	{
 		public static void Add(string key, string value)
 		{
-			AddLabel(
-				key,
-				value);
+			AddLabel(key, value);
 		}
 
 		public static void Add(string key, int value)
 		{
-			AddLabel(
-				key,
-				value);
+			AddLabel(key, value);
 		}
 
 		public static void Add(string key, double value)
 		{
-			AddLabel(
-				key,
-				value);
+			AddLabel(key, value);
 		}
 
 		public static void Add(string key, bool value)
 		{
-			AddLabel(
-				key,
-				value);
+			AddLabel(key, value);
 		}
 
 		private static void AddLabel(string key, object value)
@@ -42,17 +34,14 @@ namespace Epsagon.Dotnet.Instrumentation
 
 			Dictionary<string, object> labelsDict = new Dictionary<string, object>();
 
-			if (tags.TryGetValue(
-				"epsagon.labels",
-				out var labels))
+			if (tags.TryGetValue("epsagon.labels", out var labels))
 			{
 				labelsDict = JsonConvert.DeserializeObject<Dictionary<string, object>>(labels.ToString());
 			}
 
 			labelsDict[key] = value;
-			span.SetTag(
-				"epsagon.labels",
-				JsonConvert.SerializeObject(labelsDict));
+			
+			span.SetTag("epsagon.labels", JsonConvert.SerializeObject(labelsDict));
 		}
 	}
 }
