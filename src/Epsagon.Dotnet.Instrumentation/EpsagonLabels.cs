ad6018a2c7d3dc58b50c5b7287446f027a541bf0
James Eastham <dev@jameseastham.co.uk>
2021-02-23 16:48:27 +0000

    feat(labels): add methods for labelling with int double and bool (#36) (#38)

--- src/Epsagon.Dotnet.Instrumentation/EpsagonLabels.cs
+++ src/Epsagon.Dotnet.Instrumentation/EpsagonLabels.cs
@@ -5,21 +5,43 @@ using OpenTracing.Util;
 
 namespace Epsagon.Dotnet.Instrumentation
 {
-    public class EpsagonLabels
-    {
-        public static void Add(string key, string value)
-        {
-            if (!(GlobalTracer.Instance?.ActiveSpan is Span span)) return;
-            var tags = span.GetTags();
+	public class EpsagonLabels
+	{
+		public static void Add(string key, string value)
+		{
+			AddLabel(key, value);
+		}
 
-            Dictionary<string, string> labelsDict = new Dictionary<string, string>();
-            if (tags.TryGetValue("epsagon.labels", out var labels))
-            {
-                labelsDict = JsonConvert.DeserializeObject<Dictionary<string, string>>(labels.ToString());
-            }
+		public static void Add(string key, int value)
+		{
+			AddLabel(key, value);
+		}
 
-            labelsDict[key] = value;
-            span.SetTag("epsagon.labels", JsonConvert.SerializeObject(labelsDict));
-        }
-    }
+		public static void Add(string key, double value)
+		{
+			AddLabel(key, value);
+		}
+
+		public static void Add(string key, bool value)
+		{
+			AddLabel(key, value);
+		}
+
+		private static void AddLabel(string key, object value)
+		{
+			if (!(GlobalTracer.Instance?.ActiveSpan is Span span)) return;
+			var tags = span.GetTags();
+
+			Dictionary<string, object> labelsDict = new Dictionary<string, object>();
+
+			if (tags.TryGetValue("epsagon.labels", out var labels))
+			{
+				labelsDict = JsonConvert.DeserializeObject<Dictionary<string, object>>(labels.ToString());
+			}
+
+			labelsDict[key] = value;
+			
+			span.SetTag("epsagon.labels", JsonConvert.SerializeObject(labelsDict));
+		}
+	}
 }
---
+++ src/Epsagon.Dotnet.Instrumentation/Triggers/CloudWatchEventsLambda.cs
@@ -0,0 +1,27 @@
+using System.Linq;
+using Amazon.Lambda.CloudWatchEvents;
+using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+using OpenTracing;
+
+namespace Epsagon.Dotnet.Instrumentation.Triggers
+{
+    public class CloudWatchEventsLambda : BaseTrigger<CloudWatchEvent<object>>
+    {
+        public CloudWatchEventsLambda(CloudWatchEvent<object> input) : base(input)
+        {
+        }
+
+        public override void Handle(ILambdaContext context, IScope scope)
+        {
+            base.Handle(context, scope);
+            scope.Span.SetTag("event.id", input.Id);
+            scope.Span.SetTag("resource.type", "events");
+            scope.Span.SetTag("resource.name", input.Resources.First().Split('/').Last());
+            scope.Span.SetTag("resource.operation", input.DetailType);
+            scope.Span.SetTag("aws.events.region", input.Region);
+            scope.Span.SetTag("aws.events.detail", input.Detail.ToString());
+            scope.Span.SetTag("aws.events.account", input.Account);
+        }
+    }
+}
