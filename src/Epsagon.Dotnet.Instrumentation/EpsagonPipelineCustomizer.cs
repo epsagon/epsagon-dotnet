93b1aff8ac785275feb90e34a7cb676e9d8decca
Tal Vintrob <tal@epsagon.com>
2021-07-26 09:57:14 -0400

    wip

--- src/Epsagon.Dotnet.Instrumentation/EpsagonPipelineCustomizer.cs
+++ src/Epsagon.Dotnet.Instrumentation/EpsagonPipelineCustomizer.cs
@@ -2,6 +2,8 @@ using System;
 
 using Amazon.Runtime.Internal;
 
+using Epsagon.Dotnet.Core;
+
 namespace Epsagon.Dotnet.Instrumentation {
     public class EpsagonPipelineCustomizer : IRuntimePipelineCustomizer {
         public string UniqueName => "Epsagon pipeline customizer";
@@ -9,6 +11,8 @@ namespace Epsagon.Dotnet.Instrumentation {
         public void Customize(Type type, RuntimePipeline pipeline) {
             var handler = new HandlerFactory(type.Name).GetInstace(type);
             pipeline.AddHandler(handler);
+
+            Utils.DebugLogIfEnabled("done adding pipeline handler, type: {}", type.Name);
         }
     }
 }
