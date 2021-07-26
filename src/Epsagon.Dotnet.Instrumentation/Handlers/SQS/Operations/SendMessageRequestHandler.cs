30364ab5cf957ceb5caf8a59e632723dd2070905
Tal Vintrob <tvaintrob@users.noreply.github.com>
2021-07-26 15:06:18 -0400

    fix(sqs): fixing sqs message id not connecting (#58)

--- src/Epsagon.Dotnet.Instrumentation/Handlers/SQS/Operations/SendMessageRequestHandler.cs
+++ src/Epsagon.Dotnet.Instrumentation/Handlers/SQS/Operations/SendMessageRequestHandler.cs
@@ -13,6 +13,7 @@ namespace Epsagon.Dotnet.Instrumentation.Handlers.SQS.Operations {
         public void HandleOperationAfter(IExecutionContext context, IScope scope) {
             var response = context.ResponseContext.Response as SendMessageResponse;
 
+            _scope.Span.SetTag("aws.sqs.Message ID", response.MessageId);
             _scope.Span.SetTag("aws.sqs.message_id", response.MessageId);
             _scope.Span.SetTag("aws.sqs.message_body_md5", response.MD5OfMessageBody);
             _scope.Dispose();
